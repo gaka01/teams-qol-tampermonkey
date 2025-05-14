edd853f15868207ba38be3636bb3e2c47738a8fb
Galin Dobchev <gaka01@gmail.com>
2025-05-14 11:17:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Teams quality of life
-// @namespace    http://tampermonkey.net/
-// @version      2025-04-25
-// @description  try to take over the world!
-// @author       Galin Dobchev
-// @match        https://teams.live.com/v2/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=live.com
-// @grant        none
+// @name        Teams QOL
+// @namespace   Violentmonkey Scripts
+// @match       https://teams.live.com/v2*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 5/13/2025, 9:34:41 AM
 // ==/UserScript==
+
 
 (function() {
     'use strict';
@@ -18,11 +18,16 @@
         '.fui-Primitive:has(button[data-tid="railHeaderFilterButton"])', // Useless chat header
         'div:has(> svg[data-tid=titlebar-teams-icon])', // Teams logo
         'div:has(>div[data-tid="window-controls-placeholder-TitleBarStart"])', //Uselesss top-left blank space
+        'div:has(>div[aria-label="Communities"])', // Communities button
+        'div:has(>div[aria-label="Meet"])', // Meet button
+        'button[data-testid="premium-upgrade-button"]', // Premium (diamond) button
     ];
 
     addGlobalStyle(SELECTORS_TO_HIDE.join(', ') + ' { display: none; }');
 
     addGlobalStyle('.fui-ChatMessage__timestamp, .fui-ChatMyMessage__timestamp { display: block; }'); // always show message timestamps
+
+    //addGlobalStyle('body { --colorBrandForeground1: red; }');
 
 
     function addGlobalStyle(css, important=true) {
